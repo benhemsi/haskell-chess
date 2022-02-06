474deaf22f0e92fef5e11a07ebcfb6c211498116
benhemsi <hemsib@gmail.com>
2022-02-06 12:32:32 +0000

    Added Monoid instance for MoveTree

--- src/Moves/MoveTree.hs
+++ src/Moves/MoveTree.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE DeriveTraversable #-}
 
 module Moves.MoveTree where
-import Models.Position (Position)
+
+import Data.Foldable
 
 data MoveTree a = EmptyTree | Leaf a | Node a [MoveTree a] deriving (Eq, Functor, Foldable, Traversable)
 
@@ -10,3 +11,24 @@ filterTree predicate tree = case tree of
   EmptyTree -> EmptyTree
   Leaf a -> if predicate a then Leaf a else EmptyTree
   Node a trees -> if predicate a then Node a (filter (EmptyTree /=) (map (filterTree predicate) trees)) else EmptyTree
+
+instance Semigroup a => Semigroup (MoveTree a) where
+  tree1 <> tree2 = case (tree1, tree2) of
+    (x, EmptyTree) -> x
+    (EmptyTree, y) -> y
+    (Leaf x, Leaf y) -> Leaf (x <> y)
+    (Node x [], Node y []) -> Leaf (x <> y)
+    (Node x [EmptyTree], Node y [EmptyTree]) -> Leaf (x <> y)
+    (Node x [], Leaf y) -> Leaf (x <> y)
+    (Leaf x, Node y []) -> Leaf (x <> y)
+    (Node x [EmptyTree], Leaf y) -> Leaf (x <> y)
+    (Leaf x, Node y [EmptyTree]) -> Leaf (x <> y)
+    (Node _ [Leaf x], Leaf y) -> Leaf (x <> y)
+    (Leaf x, Node _ [Leaf y]) -> Leaf (x <> y)
+    (Node _ [Leaf x], Node _ [Leaf y]) -> Leaf (x <> y)
+    (Leaf x, Node _ trees) -> Leaf x <> foldl' (<>) EmptyTree trees
+    (Node _ trees, Leaf y) -> foldl' (<>) EmptyTree trees <> Leaf y
+    (Node _ treesA, Node _ treesB) -> foldl' (<>) EmptyTree treesA <> foldl' (<>) EmptyTree treesB
+
+instance Semigroup a => Monoid (MoveTree a) where
+  mempty = EmptyTree
