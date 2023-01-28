794fe0213f49ab79ea66a371f5c099d2df9638e2
benhemsi <hemsib@gmail.com>
2023-01-28 19:19:53 +0000

    FEN parser app working

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Data.Aeson
+import Data.ByteString.Lazy.Char8
 import Models.Fen.FenParser
+import Network.HTTP.Types
+import Network.Wai
+import Network.Wai.Handler.Warp (run)
 import System.Exit
 
-play :: IO ()
-play = do
-  let playAgain = do
-        putStrLn "Would you like to play again? (y/n)"
-        ch <- getLine
-        if ch == "y"
-          then play
-          else exitSuccess
-  putStrLn "Enter FEN:"
-  str <- getLine
-  case parseFen str of
-    Right valid -> do
-      print (encode valid)
-      playAgain
-    Left error -> do
-      print error
-      playAgain
+application req respond =
+  if requestMethod req == methodPut
+    then do
+      body <- lazyRequestBody req
+      let parsed = parseFen (unpack body)
+      case parsed of
+        Right valid -> do
+          let json = encode valid
+          respond $ responseLBS status200 [("Content-Type", "application/json")] json
+        Left error -> do
+          respond $ responseLBS status400 [("Content-Type", "text/plain")] (pack (show error))
+    else respond $ responseLBS status200 [("Content-Type", "text/plain")] "FEN Parser active"
 
-main = play
+main = run 3000 application
