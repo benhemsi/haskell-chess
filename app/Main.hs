f4bedb1180a93e51a3e2385e50c73050366c016d
benhemsi <hemsib@gmail.com>
2023-06-25 18:06:22 +0100

    App connects to postgres db and table creation works

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,27 @@
 
 module Main where
 
+import Chess.Fen (startingFenRepresentation)
 import Chess.Fen.FenParser
+import Chess.OpeningTable.OpeningTable
+import Control.Concurrent (threadDelay)
+import Control.Monad.Logger
+import Control.Monad.Reader (runReaderT)
 import Data.Aeson
-import Data.ByteString.Lazy.Char8
+import Data.ByteString.Lazy.Char8 hiding (putStrLn)
+import Database.Persist (insertKey)
+import Database.Persist.Postgresql (ConnectionString, SqlPersistT, runMigration, withPostgresqlConn)
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Handler.Warp (run)
 import System.Exit
+
+connString :: ConnectionString
+connString = "host=postgres port=5432 user=postgres dbname=postgres password=postgres"
+
+runAction :: ConnectionString -> SqlPersistT (LoggingT IO) a -> IO a
+runAction connectionString action =
+  runStdoutLoggingT $ withPostgresqlConn connectionString $ \backend -> runReaderT action backend
 
 application req respond =
   if requestMethod req == methodPut
@@ -23,4 +37,10 @@ application req respond =
           respond $ responseLBS status400 [("Content-Type", "text/plain")] (pack (show error))
     else respond $ responseLBS status200 [("Content-Type", "text/plain")] "FEN Parser active"
 
-main = run 3000 application
+main = do
+  threadDelay 1000000
+  putStrLn "Adding table"
+  runAction connString (runMigration migrateAll)
+  let (key, openingPos) = fenWithEvalToOpeningPosition startingFenRepresentation 0.0
+  putStrLn "Inserting row"
+  runAction connString (insertKey key openingPos)
