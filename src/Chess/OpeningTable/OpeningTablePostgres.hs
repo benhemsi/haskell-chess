88d8621112fc4f28cb0d78703440f9d5ba902d20
benhemsi <hemsib@gmail.com>
2023-07-01 16:40:09 +0100

    Added OpeningTable servant api

--- src/Chess/OpeningTable/OpeningTablePostgres.hs
+++ src/Chess/OpeningTable/OpeningTablePostgres.hs
@@ -2,7 +2,13 @@
 
 module Chess.OpeningTable.OpeningTablePostgres where
 
-import Database.Persist.Postgresql (ConnectionString)
+import Control.Monad.Logger
+import Control.Monad.Reader
+import Database.Persist.Postgresql
 
 connString :: ConnectionString
-connString = "host=127.0.0.1 port=5432 user=postgres dbname=postgres password=password"
+connString = "host=postgres port=5432 user=postgres dbname=postgres password=postgres"
+
+runAction :: ConnectionString -> SqlPersistT (LoggingT IO) a -> IO a
+runAction connectionString action =
+  runStdoutLoggingT $ withPostgresqlConn connectionString $ \backend -> runReaderT action backend
