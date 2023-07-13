cd6b0f5f6c4d2b95ffbc961ab11a0489c3c5ae71
benhemsi <hemsib@gmail.com>
2023-07-13 20:57:06 +0100

    Removed ChessApp

--- build_images.sh
+++ build_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 OPENING_TABLE_EXE_PATH=`stack exec -- which opening-table-exe`
-CHESS_EXE_PATH=`stack exec -- which chess-exe`
-docker-compose build --build-arg OPENING_TABLE_BINARY_PATH=$(realpath --relative-to="$PWD" "$OPENING_TABLE_EXE_PATH") --build-arg CHESS_BINARY_PATH=$(realpath --relative-to="$PWD" "$CHESS_EXE_PATH")
+# CHESS_EXE_PATH=`stack exec -- which chess-exe`
+# docker-compose build --build-arg OPENING_TABLE_BINARY_PATH=$(realpath --relative-to="$PWD" "$OPENING_TABLE_EXE_PATH") --build-arg CHESS_BINARY_PATH=$(realpath --relative-to="$PWD" "$CHESS_EXE_PATH")
+docker-compose build --build-arg OPENING_TABLE_BINARY_PATH=$(realpath --relative-to="$PWD" "$OPENING_TABLE_EXE_PATH")
