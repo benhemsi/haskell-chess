d612c5b6bccb2dd6ecae6cc1ae5ddc99c9a99118
benhemsi <hemsib@gmail.com>
2023-08-12 09:34:40 +0100

    Made defining piece weightings config optional using barbies

--- src/Chess/Evaluation/PieceWeightings.hs
+++ src/Chess/Evaluation/PieceWeightings.hs
@@ -1,27 +1,46 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Chess.Evaluation.PieceWeightings where
 
+import Barbies
 import Chess.Fen
 import Chess.OpeningTable.OpeningTableSettings (OpeningTableSettings)
 import Chess.Piece
 import Control.Lens
-import Data.Aeson (FromJSON)
+import Data.Aeson (FromJSON, parseJSON)
 import qualified Data.Map as Map
 import GHC.Generics (Generic)
 
-data PieceWeightings =
+data PieceWeightings_ f =
   PieceWeightings
-    { pawnWeight, knightWeight, bishopWeight, rookWeight, queenWeight :: Double
+    { pawnWeight, knightWeight, bishopWeight, rookWeight, queenWeight :: f Double
     }
-  deriving (Generic, Show)
+  deriving (Generic, FunctorB, TraversableB, ApplicativeB, ConstraintsB)
 
-instance FromJSON PieceWeightings
+type PieceWeightings = PieceWeightings_ Identity
+
+deriving instance
+         AllBF Show f PieceWeightings_ => Show (PieceWeightings_ f)
+
+instance FromJSON (PieceWeightings_ Maybe)
+
+instance FromJSON PieceWeightings where
+  parseJSON = fmap (pieceWeightingsWithDefault defaultPieceWeightings) . parseJSON
+
+defaultPieceWeightings :: PieceWeightings
+defaultPieceWeightings = PieceWeightings 1 3 3 5 9
+
+pieceWeightingsWithDefault :: PieceWeightings -> PieceWeightings_ Maybe -> PieceWeightings
+pieceWeightingsWithDefault = bzipWith (`maybe` pure)
 
 calculatePieceWeight :: PieceWeightings -> PieceType -> Double
 calculatePieceWeight pieceWeights piece = weight
   where
-    weight =
+    Identity weight =
       case piece of
         Pawn -> pawnWeight pieceWeights
         Knight -> knightWeight pieceWeights
