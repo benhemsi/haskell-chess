a79ca029ff191947e544fb2d0bb83b400805a976
benhemsi <hemsib@gmail.com>
2023-08-14 18:54:46 +0100

    Changed state monad to tvar to get dynamic config to work

--- src/Chess/Evaluation/EvaluationApi.hs
+++ src/Chess/Evaluation/EvaluationApi.hs
@@ -1,8 +1,8 @@
 module Chess.Evaluation.EvaluationApi where
 
-import Chess.Evaluation.PieceWeightings (PieceWeightings_)
+import Chess.Evaluation.PieceWeightings
 import Chess.Fen (FenRepresentation)
 
 class EvaluationApi m where
   evaluateFen :: FenRepresentation -> m Double
-  updatePieceWeightings :: PieceWeightings_ Maybe -> m ()
+  updatePieceWeightings :: PieceWeightings_ Maybe -> m PieceWeightings
--- src/Chess/Evaluation/EvaluationConfig.hs
+++ src/Chess/Evaluation/EvaluationConfig.hs
@@ -4,15 +4,20 @@ module Chess.Evaluation.EvaluationConfig where
 
 import Chess.Evaluation.PieceWeightings
 import Chess.OpeningTable.OpeningTableSettings (OpeningTableSettings)
+import Control.Concurrent.STM
 import Control.Lens
 
 data EvaluationConfig =
   EvaluationConfig
     { _openingTableSettings :: OpeningTableSettings
-    , _pieceWeightings :: PieceWeightings
+    , _pieceWeightings :: TVar PieceWeightings
     }
 
 makeLenses ''EvaluationConfig
 
-updatePieceWeightingsInEvalConf :: PieceWeightings_ Maybe -> EvaluationConfig -> EvaluationConfig
-updatePieceWeightingsInEvalConf newPW = over pieceWeightings (`pieceWeightingsWithDefault` newPW)
+updatePieceWeightingsInEvalConf :: PieceWeightings_ Maybe -> EvaluationConfig -> STM EvaluationConfig
+updatePieceWeightingsInEvalConf newPW currentConfig = do
+  currentPieceWeightings <- readTVar (currentConfig ^. pieceWeightings)
+  let newPieceWeightings = pieceWeightingsWithDefault currentPieceWeightings newPW
+  _ <- writeTVar (currentConfig ^. pieceWeightings) newPieceWeightings
+  return currentConfig
